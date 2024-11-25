9731febcd66398786124fbbd60a7bf825faf5047
SvetaKrivalova <105561780+SvetaKrivalova@users.noreply.github.com>
2024-11-25 16:32:56 +0700

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,31 @@
+// вкладки
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, Tab) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Tab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+
+
+
 function openLoadingScreen() {
     var LoadingScreen = document.querySelector('.LoadingScreen');
     document.querySelector('.overlay').classList.add('show');
